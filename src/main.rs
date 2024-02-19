a09ab9104a069465cf83c61e6f5a9ab6eef28935
Szymon Witamborski <szrw@longstandingbug.com>
2024-02-19 21:32:09 +0900

    Add volume +/-

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-use std::cmp;
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::{cmp, env};
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
     let mut stream = TcpStream::connect("192.168.0.2:50001")?;
-    let volume = change_volume(&mut stream, -3)?;
-    println!("{volume}");
+
+    match command.as_str() {
+        "volume" => {
+            let volume = if args.len() > 2 {
+                let amount = match i8::from_str_radix(&args[2], 10) {
+                    Ok(n) => n,
+                    Err(x) => panic!("asdf"),
+                };
+                change_volume(&mut stream, amount)?
+            } else {
+                get_volume(&mut stream)?
+            };
+            println!("{volume:?}");
+        }
+        _ => {}
+    }
+
     stream.shutdown(std::net::Shutdown::Both)?;
     Ok(())
 }
@@ -23,13 +40,11 @@ fn change_volume(stream: &mut TcpStream, amount: i8) -> std::io::Result<u8> {
 
 fn get_volume(stream: &mut TcpStream) -> std::io::Result<u8> {
     let bytes = send(stream, &[0x47, 0x25, 0x80])?;
-    println!("{bytes:?}");
     Ok(bytes[3])
 }
 
 fn set_volume(stream: &mut TcpStream, volume: u8) -> std::io::Result<u8> {
-    let bytes = send(stream, &[0x53, 0x25, 0x81, volume])?;
-    println!("{bytes:?}");
+    send(stream, &[0x53, 0x25, 0x81, volume])?;
     Ok(volume)
 }
 
@@ -37,5 +52,6 @@ fn send(stream: &mut TcpStream, cmd: &[u8]) -> std::io::Result<[u8; 16]> {
     let mut result = [0; 16];
     stream.write(&cmd)?;
     stream.read(&mut result)?;
+    println!("{result:?}");
     Ok(result)
 }
