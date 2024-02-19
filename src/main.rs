e733c8a6515e37ccce43fed7db706eeebdf64b2f
Szymon Witamborski <szrw@longstandingbug.com>
2024-02-19 20:51:44 +0900

    Add change_volume, hopefully overflow-safe

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
+use std::cmp;
 use std::io::prelude::*;
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
-    let volume = get_volume()?;
-    println!("{volume:?}");
+    let mut stream = TcpStream::connect("192.168.0.2:50001")?;
+    let volume = change_volume(&mut stream, -3)?;
+    println!("{volume}");
+    stream.shutdown(std::net::Shutdown::Both)?;
     Ok(())
 }
 
-fn get_volume() -> std::io::Result<u8> {
-    let bytes = send([0x47, 0x25, 0x80])?;
-    println!("{bytes:?}");
-    Ok(bytes[0])
+fn change_volume(stream: &mut TcpStream, amount: i8) -> std::io::Result<u8> {
+    let mut volume = get_volume(stream)?;
+    volume = if amount < 0 {
+        volume - cmp::min(-amount as u8, volume)
+    } else {
+        volume + cmp::min(amount as u8, 100 - volume)
+    };
+    set_volume(stream, volume)?;
+    Ok(volume)
 }
 
-fn send(cmd: [u8; 3]) -> std::io::Result<[u8; 32]> {
-    let mut stream = TcpStream::connect("192.168.0.2:50001")?;
-    let mut result = [0; 32];
+fn get_volume(stream: &mut TcpStream) -> std::io::Result<u8> {
+    let bytes = send(stream, &[0x47, 0x25, 0x80])?;
+    println!("{bytes:?}");
+    Ok(bytes[3])
+}
+
+fn set_volume(stream: &mut TcpStream, volume: u8) -> std::io::Result<u8> {
+    let bytes = send(stream, &[0x53, 0x25, 0x81, volume])?;
+    println!("{bytes:?}");
+    Ok(volume)
+}
+
+fn send(stream: &mut TcpStream, cmd: &[u8]) -> std::io::Result<[u8; 16]> {
+    let mut result = [0; 16];
     stream.write(&cmd)?;
     stream.read(&mut result)?;
     Ok(result)
