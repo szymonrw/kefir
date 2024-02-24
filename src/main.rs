eb0a0f281fb6dcbf4da968b41a854ca3a3882873
Szymon Witamborski <szrw@longstandingbug.com>
2024-02-24 15:44:05 +0900

    Add both +/- and absolute values for volume control

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,20 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::{cmp, env};
 
-fn main() -> Result<(), std::io::Error> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let command = if args.len() > 1 { args[1].as_str() } else { "" };
     let mut stream = TcpStream::connect("192.168.0.2:50001")?;
 
     match command {
-        "volume" => {
-            let volume = if args.len() > 2 {
-                let amount = match i8::from_str_radix(&args[2], 10) {
-                    Ok(n) => n,
-                    Err(x) => panic!("asdf"),
-                };
+        "volume" | "vol" | "v" => {
+            let volume = if args.len() > 3 {
+                let sign: i8 = if args[2] == "-" { -1 } else { 1 };
+                let amount = sign * i8::from_str_radix(&args[3], 10)?;
                 change_volume(&mut stream, amount)?
+            } else if args.len() > 2 {
+                let volume = u8::from_str_radix(&args[2], 10)?;
+                set_volume(&mut stream, volume)?
             } else {
                 get_volume(&mut stream)?
             };
@@ -49,14 +50,15 @@ fn get_volume(stream: &mut TcpStream) -> std::io::Result<u8> {
 }
 
 fn set_volume(stream: &mut TcpStream, volume: u8) -> std::io::Result<u8> {
-    send(stream, &[0x53, 0x25, 0x81, volume])?;
-    Ok(volume)
+    let corrected = volume.clamp(0, 100);
+    send(stream, &[0x53, 0x25, 0x81, corrected])?;
+    Ok(corrected)
 }
 
-fn send(stream: &mut TcpStream, cmd: &[u8]) -> std::io::Result<[u8; 16]> {
-    let mut result = [0; 16];
+fn send(stream: &mut TcpStream, cmd: &[u8]) -> std::io::Result<[u8; 8]> {
+    let mut result = [0; 8];
     stream.write(&cmd)?;
     stream.read(&mut result)?;
-    println!("{result:?}");
+    println!("{cmd:?}<->{result:?}");
     Ok(result)
 }
